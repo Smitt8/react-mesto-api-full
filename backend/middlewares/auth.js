14e849940a18f2df040fbf17a6ccce24365cf8d4
Igor Prokazov <smitt8@yandex.ru>
2022-09-27 01:19:05 +0300

    fix: Исправить ошибку jwt

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,12 +1,14 @@
 const jwt = require('jsonwebtoken');
 const ErrorAuth = require('../utils/ErrorAuth');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     next(new ErrorAuth('Пожалуйста авторизуйтесь.'));
   }
